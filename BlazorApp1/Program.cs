3dac230360a3b58b2dc4bc9c5bbe8bf1af81c88a
Luis Carlos Perez <lperez@estrategiasdocumentales.com>
2022-09-29 13:02:58 -0500

    test

---
+++ BlazorApp1/Program.cs
@@ -0,0 +1,10 @@
+using BlazorApp1;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
